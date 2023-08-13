bd05c1b56d2782568a980f9ebdde928a542fc3e4
Abhik Das <abhikdas98765@gmail.com>
2023-08-13 21:47:22 +0530

    Added social login using google

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -86,7 +86,7 @@ const LoginModal = () => {
                 outline
                 label="Continue with Google"
                 icon={FcGoogle}
-                onClick={() => {}}
+                onClick={() => signIn("google")}
             />
             <Button
                 outline
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -89,7 +89,7 @@ const RegisterModal = () => {
                 outline
                 label="Continue with Google"
                 icon={FcGoogle}
-                onClick={() => {}}
+                onClick={() => signIn("google")}
             />
             <Button
                 outline
