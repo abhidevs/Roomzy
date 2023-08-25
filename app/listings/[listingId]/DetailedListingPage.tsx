cda29ead67dd30f1ca194c4b341bb60d5d18c1e5
Abhik Das <abhikdas98765@gmail.com>
2023-08-25 09:33:17 +0530

    Fixed total price showing in negative

--- app/listings/[listingId]/DetailedListingPage.tsx
+++ app/listings/[listingId]/DetailedListingPage.tsx
@@ -87,8 +87,8 @@ const DetailedListingPage: React.FC<DetailedListingPageProps> = ({
     useEffect(() => {
         if (dateRange.startDate && dateRange.endDate) {
             const dayCount = differenceInCalendarDays(
-                dateRange.startDate,
-                dateRange.endDate
+                dateRange.endDate,
+                dateRange.startDate
             );
 
             if (dayCount && listing.price) {
