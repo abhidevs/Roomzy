9df675a2681e7979a5cd32e5a3337a7ccb54691b
Abhik Das <abhikdas98765@gmail.com>
2023-08-30 11:10:31 +0530

    Fixed deployment error with apostrophe in production

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -101,7 +101,7 @@ const LoginModal = () => {
             />
             <div className="text-neutral-500 text-center mt-4 font-light">
                 <div className="flex flex-row items-center justify-center gap-2">
-                    <div>Don't have an account?</div>
+                    <div>Don&apos;t have an account?</div>
                     <div
                         onClick={switchToRegisterModal}
                         className="text-neutral-800 cursor-pointer hover:underline"
