a180f5879e7c064fdc3a11230f39d46b2e431cfd
Abhik Das <abhikdas98765@gmail.com>
2023-08-30 11:37:45 +0530

    Added force-dynamic route segment fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingParams } from "./actions/getListings";
 import Container from "./components/Container";
